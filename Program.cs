f8cea68f35510964b2febd7aa3d09e465b52172b
Natalya <vladislava2006@mail.ru>
2023-02-23 16:24:26 +0300

    Add Final project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+
+string[] array1 = new string[7] { "9", "five", "27", "377", "ten", "3762", "one" };
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+Console.WriteLine();
+Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символа: ");
+PrintArray(array2);
+Console.WriteLine();
